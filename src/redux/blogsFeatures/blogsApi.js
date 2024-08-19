6098f55b41d57229c6b08a5386fccc843768b222
12imthi <110447839+12imthi@users.noreply.github.com>
2024-08-19 12:34:35 +0530

    testing

--- src/redux/blogsFeatures/blogsApi.js
+++ src/redux/blogsFeatures/blogsApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const blogApi = createApi({
   reducerPath: "blogsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/",
+    baseUrl: "https://bloging-backend-1.onrender.com/api/",
     credentials: "include",
   }),
   tagTypes: ["Blogs"],
