f1298a8b89ee2324fb9a7435eb6a141a71a85b5a
12imthi <110447839+12imthi@users.noreply.github.com>
2024-10-01 12:24:45 +0530

    add

--- src/pages/admin/post/ManagePost.jsx
+++ src/pages/admin/post/ManagePost.jsx
@@ -54,7 +54,7 @@ function ManagePost() {
           <UserDashboard />
         </div>
       ) : (
-        <div></div>
+        <div> no post</div>
       )}
       {isLoading && <p className="text-center py-4">Loading...</p>}
       {error && (
@@ -147,7 +147,7 @@ function ManagePost() {
           </div>
         </section>
       ) : (
-        <div></div>
+        <div> no post</div>
       )}
     </>
   );
